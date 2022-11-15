5f66ffbc9b137534399123baf621bb151ffdf299
laibasarwar <sarwar.laiba@gmail.com>
2022-11-15 09:49:51 -0500

    added login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import { useEffect, useState } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import React from "react";
+import { initializeApp } from "firebase/app";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import "./App.css";
-
 import CreateUserPage from "./pages/CreateUser";
 import LoginPage from "./pages/Login";
 import UserProfilePage from "./pages/UserProfile";
-
 import Header from "./components/Header";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDUxTjaE8fLD1woMlMnPBUbiIlmSql2caA",
+  authDomain: "exercise-six-32526.firebaseapp.com",
+  projectId: "exercise-six-32526",
+  storageBucket: "exercise-six-32526.appspot.com",
+  messagingSenderId: "906927206491",
+  appId: "1:906927206491:web:4d3e5b137528a4cb220f7e",
+};
 
 const router = createBrowserRouter([
   {
@@ -24,6 +33,31 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
+  const [appInitialized, setAppInitialized] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userInformation, setUserInformation] = useState({});
+
+  useEffect(() => {
+    initializeApp(firebaseConfig);
+    setAppInitialized(true);
+  }, []);
+
+  useEffect(() => {
+    if (appInitialized) {
+      const auth = getAuth();
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          setUserInformation(user);
+          setIsLoggedIn(true);
+        } else {
+          setUserInformation({});
+          setIsLoggedIn(false);
+        }
+        setIsLoading(false);
+      });
+    }
+  }, [appInitialized]);
   return (
     <div className="App">
       <Header />
